7335aebe9fb7c9b9cf1234ff1a22648b9826d7b6
Dragonlord8781 <evan.stone638@gmail.com>
2025-05-05 10:40:26 -0500

    1.2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the vehicle foward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
